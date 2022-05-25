dd474bbfa4db04cdae0b046945c38708e0351a5b
White Nguyen <whitenguyen0909@gmail.com>
2022-05-25 08:02:30 -0600

    making initial files and data for the server structure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+// TODO: Follow instructions in the checkpoint to implement this API.
 
 // Not found handler
 app.use((request, response, next) => {
