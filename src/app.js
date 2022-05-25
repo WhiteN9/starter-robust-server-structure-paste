b093fb3a44a5613dd585bba201621c3ddb53f457
White Nguyen <whitenguyen0909@gmail.com>
2022-05-25 12:12:06 -0600

    express.json() is needed to read json

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const pastes = require("./data/pastes-data");
 
-app.use("/pastes/:pasteId", (req, res, next) => {
+app.use(express.json());
+
+app.get("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
+
+app.get("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
 
@@ -13,12 +19,24 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   }
 });
 
-app.use("/pastes", (req, res) => {
-  res.json({ data: pastes });
-});
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
-app.use("/", (req, res, next) => {
-  res.send("hello");
+app.post("/pastes", (req, res, next) => {
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
+    req.body;
+  const newPaste = {
+    id: ++lastPasteId, // Increment last ID, then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+  pastes.push(newPaste);
+  res.status(201).json({ data: newPaste });
 });
 
 // Not found handler
