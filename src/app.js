ba31218b0839d1c004ff25f871b56785b27f2e50
White Nguyen <whitenguyen0909@gmail.com>
2022-05-26 14:05:50 -0600

    added list, read, create function to controller and router

--- src/app.js
+++ src/app.js
@@ -2,17 +2,7 @@ const express = require("express");
 const app = express();
 const pastes = require("./data/pastes-data");
 
-// Middleware functions
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next(); // Call `next()` without an error message if the result exists
-  }
-  next({
-    status: 400,
-    message: "A 'text' property is required.",
-  });
-}
+const pastesRouter = require("./pastes/pastes.router");
 
 app.use(express.json());
 //The express.json() function is a built-in middleware that adds a body property to the request (req.body)
@@ -20,41 +10,9 @@ app.use(express.json());
 //or it will return an empty object ({}) if there was no body to parse,
 //the Content-Type wasn't matched, or an error occurred.
 
-app.get("/pastes", (req, res) => {
-  res.json({ data: pastes });
-});
+app.use("/pastes", pastesRouter);
 
-app.get("/pastes/:pasteId", (req, res, next) => {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
-
-  if (foundPaste) {
-    res.json({ data: foundPaste });
-  } else {
-    next(`Paste id not found: ${pasteId}`);
-  }
-});
-
-// Variable to hold the next ID
-// Because some IDs may already be used, find the largest assigned ID
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
-
-app.post("/pastes", bodyHasTextProperty, (req, res) => {
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
-    req.body;
-
-  const newPaste = {
-    id: ++lastPasteId, // Increment last ID, then assign as the current ID
-    name,
-    syntax,
-    exposure,
-    expiration,
-    text,
-    user_id,
-  };
-  pastes.push(newPaste);
-  res.status(201).json({ data: newPaste });
-});
+app.get("/pastes/:pasteId", pastesRouter);
 
 // Not found handler
 app.use((request, response, next) => {
--- src/pastes/pastes.controller.js
+++ src/pastes/pastes.controller.js
@@ -4,6 +4,56 @@ function list(req, res) {
   res.json({ data: pastes });
 }
 
+function read(req, res, next) {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next({
+      status: 400,
+      message: `Paste id not found: ${pasteId}`,
+    });
+  }
+}
+
+//enhance it
+function bodyHasTextProperty(req, res, next) {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    return next(); // Call `next()` without an error message if the result exists
+  }
+  next({
+    status: 400,
+    message: "A 'text' property is required.",
+  });
+}
+
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+function create(req, res) {
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
+    req.body;
+
+  const newPaste = {
+    id: ++lastPasteId, // Increment last ID, then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+  pastes.push(newPaste);
+  res.status(201).json({ data: newPaste });
+}
+
 module.exports = {
   list,
+  read,
+  //create exports an array of
+  //the middleware function bodyHasTextProperty()
+  //and the create route handler.
+  create: [bodyHasTextProperty, create],
 };
